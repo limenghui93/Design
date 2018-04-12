ee39ab9b7be981f030a2b5ee8140a9d2d85533db
limenghui <limenghui93@gmail.com>
2018-04-12 22:31:10 +0800

    动态代理 增加预处理 和 后置处理

---
+++ src/main/java/com/lmh/design/proxy/demo3/DynamicProxy.java
@@ -0,0 +1,7 @@
+package com.lmh.design.proxy.demo3;
+
+/**
+ * Created by lixiansheng on 2018/4/12.
+ */
+public class DynamicProxy {
+}
---
+++ src/main/java/com/lmh/design/proxy/demo4/Subject.java
@@ -0,0 +1,7 @@
+package com.lmh.design.proxy.demo4;
+
+/**
+ * Created by lixiansheng on 2018/4/12.
+ */
+public interface Subject {
+}
